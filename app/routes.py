71ae6698d5a30f933be5626bd6469e81578c7ddb
Dinakar Nayak. N <122340791+Dinakarnayak@users.noreply.github.com>
2024-11-08 10:36:33 +0530

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -10,14 +10,12 @@ def home():
 
 @app_routes.route('/api/translate', methods=['POST'])
 def api_translate():
-    text = request.json.get('text')
-    source_language = request.json.get('source_language')
-    target_language = request.json.get('target_language')
+    data = request.json
+    text = data.get('text')
+    source_language = data.get('source_language')
+    target_language = data.get('target_language')
     
-    # Translate text
     translated_text = translate_text(text, target_language)
-    
-    # Generate speech for the translated text
     audio_file_path = text_to_speech(translated_text, target_language)
     
     return jsonify({
